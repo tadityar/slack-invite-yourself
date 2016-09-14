eb16771afabe347faa0f1862dff5f560d22090d4
Tasya Aditya Rukmana <tadityar@gmail.com>
2016-09-14 12:19:43 +0800

    Add root and invited routes

---
+++ myapp.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'open-uri'
+require 'nokogiri'
+
+get '/' do
+  erb :index
+end
+
+post '/invite' do
+  if params[:email]
+    token = [REPLACE-WITH-YOUR-APPS-TOKEN-HERE]
+    channels = [REPLACE-WITH-THE-CHANNELS-CODES-HERE] #separated with commas
+    response = open("https://slack.com/api/users.admin.invite?token=#{token}&channels=#{channels}&email=#{params[:email]}&first_name=Example&set_active=true").read
+    erb :invited, :locals => {'email' => params[:email]}
+  else
+    erb :index
+  end
+end
